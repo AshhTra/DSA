91b69fabbcfde808d36882f8a3f3494d6a757609
Priya Pandey <146580004+AshhTra@users.noreply.github.com>
2026-08-10 11:31:19 +0530

    Time: 0 ms (100%), Space: 9.2 MB (40.91%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        if (nums.empty()) return ans;
+
+        int start = nums[0];
+        int end = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == end + 1) {
+                end = nums[i];
+            }
+            else {
+                if (start == end) {
+                    ans.push_back(to_string(start));
+                }
+                else {
+                    ans.push_back(to_string(start) + "->" + to_string(end));
+                }
+
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+
+        if (start == end) {
+            ans.push_back(to_string(start));
+        }
+        else {
+            ans.push_back(to_string(start) + "->" + to_string(end));
+        }
+
+        return ans;
+    }
+};
