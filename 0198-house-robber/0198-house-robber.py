c323a98247b9a2f110d0be0c95c0edce7d3de756
Priya Pandey <146580004+AshhTra@users.noreply.github.com>
2026-08-13 07:59:58 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,14 @@
 class Solution:
-    def f(self, i, nums, dp) -> int:
-        if i == 0: 
-            return nums[0]
-        if i < 0:
-            return 0
-        if dp[i] != -1:
-            return dp[i]
-        
-        pick = nums[i] + self.f(i - 2, nums, dp)
-        npick = self.f(i - 1, nums, dp)
-
-        dp[i] = max(pick, npick)
-        return dp[i]
-
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        return self.f(n - 1, nums, dp)
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+
+        for i in range(2, n + 1):
+            pick = nums[i - 1] + dp[i - 2]
+            npick = dp[i - 1]
+            dp[i] = max(pick, npick)
+
+        return dp[n]
 
         
