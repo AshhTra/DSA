2cc57815d4a46a32a5985b66655e44892bc8d779
Priya Pandey <146580004+AshhTra@users.noreply.github.com>
2026-08-13 08:37:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,26 +1,19 @@
-class Solution:
-    def f(self, i, nums, dp) -> int:
-        if i == 0:
-            return nums[0]
-        if i < 0:
-            return 0
-        if dp[i] != -1:
-            return dp[i]
-        
-        p = nums[i] + self.f(i - 2, nums, dp)
-        np = self.f(i - 1, nums, dp)
-
-        dp[i] = max(p, np)
-        return dp[i]
-        
+class Solution:   
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
 
         a1 = nums[:-1]
+        dp1 = [0] * (len(a1) + 1)
+        dp1[1] = a1[0]
+        for i in range(2, len(a1) + 1):
+            dp1[i] = max(a1[i - 1] + dp1[i - 2], dp1[i - 1])
+
         a2 = nums[1:]
+        dp2 = [0] * (len(a2) + 1)
+        dp2[1] = a2[0]
+        for i in range(2, len(a2) + 1):
+            dp2[i] = max(a2[i - 1] + dp2[i - 2], dp2[i - 1])
 
-        dp1 = [-1] * len(a1)
-        dp2 = [-1] * len(a2)
 
-        return max(self.f(len(a1) - 1, a1, dp1), self.f(len(a2) - 1, a2, dp2))
+        return max(dp1[len(a1)], dp2[len(a2)])
