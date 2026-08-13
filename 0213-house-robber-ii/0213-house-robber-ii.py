97c96ddb2c55218838be1e4877df574af710b423
Priya Pandey <146580004+AshhTra@users.noreply.github.com>
2026-08-13 08:44:59 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,16 +4,22 @@ class Solution:
             return nums[0]
 
         a1 = nums[:-1]
-        dp1 = [0] * (len(a1) + 1)
-        dp1[1] = a1[0]
+        p1 = a1[0]
+        p2 = 0
+        c1 = a1[0]
         for i in range(2, len(a1) + 1):
-            dp1[i] = max(a1[i - 1] + dp1[i - 2], dp1[i - 1])
+            c1 = max(a1[i - 1] + p2, p1)
+            p2 = p1
+            p1 = c1
 
         a2 = nums[1:]
-        dp2 = [0] * (len(a2) + 1)
-        dp2[1] = a2[0]
+        p1 = a2[0]
+        p2 = 0
+        c2 = a2[0]
         for i in range(2, len(a2) + 1):
-            dp2[i] = max(a2[i - 1] + dp2[i - 2], dp2[i - 1])
+            c2 = max(a2[i - 1] + p2, p1)
+            p2 = p1
+            p1 = c2
 
 
-        return max(dp1[len(a1)], dp2[len(a2)])
+        return max(c1, c2)
