1a299ccb800ae82b7196f8aba809357f55b9ea41
Priya Pandey <146580004+AshhTra@users.noreply.github.com>
2026-08-20 02:32:42 +0530

    Time: 71 ms (36.9%), Space: 73.3 MB (36.23%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,31 +1,31 @@
 class Solution {
-    bool f(int row, int col, unordered_map<int, unordered_set<int>>& seat){
-        if(seat[row].find(col) != seat[row].end() || 
-            seat[row].find(col + 1) != seat[row].end() || 
-            seat[row].find(col + 2) != seat[row].end() || 
-            seat[row].find(col + 3) != seat[row].end()){
-                return false;
-            }
-        return true;
-    }
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, unordered_set<int>>seat;
-        for(auto it : reservedSeats){
-            if(it[1] == 1 || it[1] == 10) continue;
-            seat[it[0]].insert(it[1]);
+        unordered_map<int, vector<int>> seat;
+        for (auto it : reservedSeats) {
+            if (it[1] == 1 || it[1] == 10) continue;
+            seat[it[0]].push_back(it[1]);
         }
 
         long long ans = 2LL * n;
         for (auto& [row, seats] : seat) {
             ans -= 2;
 
-            if (f(row, 2, seat) ||
-                     f(row, 4, seat) ||
-                     f(row, 6, seat)) {
+            bool left = true;    // 2,3,4,5
+            bool middle = true;  // 4,5,6,7
+            bool right = true;   // 6,7,8,9
+
+            for (int col : seats) {
+                if (col >= 2 && col <= 5) left = false;
+                if (col >= 4 && col <= 7) middle = false;
+                if (col >= 6 && col <= 9) right = false;
+            }
+
+            if (left || middle || right) {
                 ans += 1;
             }
         }
+
         return ans;
     }
 };
